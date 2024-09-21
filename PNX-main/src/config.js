a5d111615dec57fc565e6cf0ddad97d34c35da9c
eladlavi29 <47532871+eladlavi29@users.noreply.github.com>
2024-09-21 18:21:01 +0300

    Delete PNX-main directory

--- PNX-main/src/config.js
+++
@@ -1,5 +0,0 @@
-const config = {
-    mapServerUrl: 'http://localhost:8080/tile/{z}/{x}/{y}.png',
-  };
-  
-  export default config;
--- PNX-main/src/icons/Icons.ts
+++
@@ -1,8 +0,0 @@
-import L from "leaflet";
-
-export const Plane = new L.Icon({
-  iconUrl: "./plane.png",
-  iconRetinaUrl: "./plane.png",
-  iconSize: new L.Point(70, 70),
-  className: "leaflet-div-icon",
-});
